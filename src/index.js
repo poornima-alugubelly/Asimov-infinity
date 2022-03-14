051fcaea35a8b0de048940c46e4665c798518ece
poornima-alugubelly <reddypoornima683@gmail.com>
2022-03-14 16:50:46 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
 import App from "./App";
 import { makeServer } from "./server";
 
@@ -8,8 +8,8 @@ import { makeServer } from "./server";
 makeServer();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
