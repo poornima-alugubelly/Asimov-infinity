57cf9edee2e5c267d1ac103c82a1be6e447c63cb
poornima-alugubelly <reddypoornima683@gmail.com>
2022-03-16 12:39:06 +0530

    fix: recreated file

--- src/pages/product-listing/index.js
+++ src/pages/product-listing/index.js
@@ -1,16 +1,20 @@
 import { Filters } from "../../components/filters";
 import "./product-listing.css";
 import { useProductListing } from "../../context/ProductListingContext.js";
-import { getProductListService } from "../../services/getProductListService";
+import { getProductListService } from "../../services/getProductListService.js";
 import { useEffect } from "react";
 import { ProductCard } from "../../components/product-card";
+import { actionTypes } from "../../reducers/actionTypes";
 const ProductListing = () => {
 	const { productListingState, productListingDispatch } = useProductListing();
 	useEffect(() => {
 		(async () => {
 			const products = await getProductListService();
 			if (products)
-				productListingDispatch({ type: "LOAD_DATA", payload: products });
+				productListingDispatch({
+					type: actionTypes.LOAD_DATA,
+					payload: products,
+				});
 		})();
 	}, []);
 	return (
