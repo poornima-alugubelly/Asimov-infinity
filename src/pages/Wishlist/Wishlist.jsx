10a1346f38795d48e7da32bc1b49d3ca602a6de5
poornima-alugubelly <reddypoornima683@gmail.com>
2022-04-05 11:32:53 +0530

    fix: wishlist

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,15 +1,14 @@
 import { useUserData } from "../../context/UserDataContext";
 import { ProductCard } from "../ProductListing/components/ProductCard/ProductCard";
-import { useNavigate } from "react-router-dom";
 import { Loader } from "../../components/Loader/Loader";
 export const WishList = () => {
 	const { userData, error, loading } = useUserData();
 	const { wishlistProducts } = userData;
-	const navigate = useNavigate();
+
 	return !loading ? (
 		<div className="cart-page-container">
 			<h2 className="padding-l text-center txt-high-light">My Wish List</h2>
-			{WishList.length ? (
+			{wishlistProducts.length ? (
 				<div className="grid-product-layout">
 					{wishlistProducts.map((product) => (
 						<ProductCard key={product._id} product={product} />
